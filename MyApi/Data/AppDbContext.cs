2b150ae238b7ae30d59c5219aca07205d0d74a12
mattywalsh044 <Matty.walsh@soprasteria.com>
2025-10-14 08:08:43 +0000

    data namespace

--- MyApi/Data/AppDbContext.cs
+++ MyApi/Data/AppDbContext.cs
@@ -1,10 +1,13 @@
 using Microsoft.EntityFrameworkCore;
 using MyApi.Models;
 
-public class AppDbContext : DbContext
+namespace MyApi.Data
 {
-    public AppDbContext(DbContextOptions<AppDbContext> options)
-        : base(options) { }
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options) { }
 
-    public DbSet<User> Users { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
 }
